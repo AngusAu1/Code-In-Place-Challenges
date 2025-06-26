ecb4ae9923694ded996e665f58965830c11986a8
AngusAu1 <pennyau1@gmail.com>
2025-06-26 12:56:07 +0100

    Add explanation_02.md to Challenge_02

---
+++ challenges/challenge_02/challenge_02_Hello_Name.py
@@ -0,0 +1,17 @@
+"""
+Hello Name - Classic Hellow World type python coding
+"""
+
+def main():
+    """
+    You should write your code here. Make sure to delete 
+    the 'pass' line before starting to write your own code.
+    """
+
+    # defined name and assign the ipnut name by end user
+    # and then using print to display the name
+    name = input("What is your name? ")
+    print("Hello " + name)
+
+if __name__ == '__main__':
+    main()
